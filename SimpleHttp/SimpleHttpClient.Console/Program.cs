ded2c937a9e5b6b545e8e60e2aa1678129e71d0e
Krich Ayyoub <ayyoub.krich@gmail.com>
2024-09-14 22:52:45 +0200

    feat: (console) add new console project for tests

---
+++ SimpleHttp/SimpleHttpClient.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ServiceCollection serviceCollection = new ServiceCollection();
+serviceCollection.AddSimpleHttpClient("https://evilinsult.com");
+ServiceProvider services = serviceCollection.BuildServiceProvider();
+
+ISimpleHttpClient simpleHttpClient = services.GetRequiredService<ISimpleHttpClient>();
+
+InsultDto? result = await simpleHttpClient.GetAsync<InsultDto>("generate_insult.php?lang=fr&type=json");
+
+Console.WriteLine($"{JsonConvert.SerializeObject(result)}");
