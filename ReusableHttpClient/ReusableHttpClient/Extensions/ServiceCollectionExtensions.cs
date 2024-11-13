977b81a5bf049def784f8923db871b2034a81451
Krich Ayyoub <ayyoub.krich@gmail.com>
2024-11-13 14:26:49 +0100

    feat: (client) add factory to allow multiple http clients

--- ReusableHttpClient/ReusableHttpClient/Extensions/ServiceCollectionExtensions.cs
+++ ReusableHttpClient/ReusableHttpClient/Extensions/ServiceCollectionExtensions.cs
@@ -2,11 +2,11 @@
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddReusableHttpClient(this IServiceCollection services, string baseAddress)
+    public static void AddReusableHttpClient(this IServiceCollection services, string name, string baseAddress)
     {
-        services.AddHttpClient<IReusableHttpClient, Services.ReusableHttpClient>(client =>
-        {
-            client.BaseAddress = new Uri(baseAddress);
-        });
+        services.AddHttpClient();
+
+        services.AddHttpClient<IReusableHttpClient, Services.ReusableHttpClient>(name,
+            client => { client.BaseAddress = new Uri(baseAddress); });
     }
 }
